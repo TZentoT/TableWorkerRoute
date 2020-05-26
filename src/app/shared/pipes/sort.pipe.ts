d4682fdbcb3acb597e12f614dd074c12be175a21
Dmitriy Borisenko <uc00510144@yandex.ru>
2020-05-26 16:25:28 +0300

    v1.3

--- src/app/shared/pipes/sort.pipe.ts
+++ src/app/shared/pipes/sort.pipe.ts
@@ -9,34 +9,33 @@ export class SortPipe implements PipeTransform {
 
   transform(workerList: MyWorkers[], filterID: number, filterAge): any {
 
-    let res: MyWorkers[] = workerList;
     let age = new GetAgeService
 
     if (filterID == 1) {
-      res.sort(function(a,b){
+      workerList.sort(function(a,b){
         return a.id - b.id
       })
     }
     
     if (filterID == 2){
-      res.sort(function(a,b){
+      workerList.sort(function(a,b){
         return b.id - a.id
       })
     }
 
     if (filterAge == 1){
-      res.sort(function(a,b){
+      workerList.sort(function(a,b){
         return age.getAge(a.birthday) - age.getAge(b.birthday);
       })
     }
 
     if (filterAge == 2){
-      res.sort(function(a,b){
+      workerList.sort(function(a,b){
         return age.getAge(b.birthday) - age.getAge(a.birthday);
       })
     }
 
-    return res;
+    return workerList;
   }
 }
 
